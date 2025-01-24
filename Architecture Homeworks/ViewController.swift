4ae2cc4bf5f58d8bd6d631530e7dd241668f3ee1
Artem Kalugin <out_KaluginA@mkb.ru>
2025-01-24 19:19:24 +0300

    Initial Commit

---
+++ Architecture Homeworks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Architecture Homeworks
+//
+//  Created by Artem Kalugin on 24.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
