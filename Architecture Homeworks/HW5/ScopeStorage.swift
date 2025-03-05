ecdf8b948742ff2d7a52b872df04a902037abc14
Artem Kalugin <out_KaluginA@mkb.ru>
2025-03-05 20:22:24 +0300

    Homework5

---
+++ Architecture Homeworks/HW5/ScopeStorage.swift
@@ -0,0 +1,15 @@
+//
+//  ScopeStorage.swift
+//  Architecture Homeworks
+//
+//  Created by Artem Kalugin on 02.03.2025.
+//
+
+import Foundation
+
+final class ScopeStorage {
+    
+    // MARK: - Properties
+    
+    var storage = [String: (([Any]) -> AnyObject)]()
+}
